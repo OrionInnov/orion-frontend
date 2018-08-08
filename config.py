ecb6d5726f4995787c4672a327fffc6d1270ff8e
guyunshan <1964100035@qq.com>
2018-08-08 15:02:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,23 @@
 import json
 import pymongo
+
+test = [[1, 2, 3], [2, 3, 1], [2, 4, 5], [3, 2, 3]]
 client = pymongo.MongoClient('mongodb://localhost:27017')
 db = client['orion']
 
+def save_history():
+    num = 1
+    posdic = {}
+    import time
+    time = time.time()
+    while num <= len(test):
+        posdic[str(num)] = test[num - 1]
+        num = num + 1
+
+    posdic["time"] = time
+    print(posdic)
+    db.history.insert(posdic)
+    
 
 def load_config():
     cursor = db.config.find()
