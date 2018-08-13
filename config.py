ea05f3eb65da13829dc278ff063dc7c301aa723c
guyunshan <1964100035@qq.com>
2018-08-13 14:43:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,47 @@
+# coding:utf-8
 import json
 import pymongo
 
+# MongoDB client URL and port
+DEFAULT_URL = "mongodb://localhost:27017"
 
-test = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-        [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-data = {"tag": 6, "location": [1, 2, 3]}
-def change_pos(data, test):
-    test[data["tag"]] = data["location"]
-    print(test)
-
-client = pymongo.MongoClient('mongodb://localhost:27017')
+client = pymongo.MongoClient(DEFAULT_URL)
 db = client['orion']
 
-def save_history():
+
+# test = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
+#        [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+poslist = []
+data = {"tag": 1, "location": [1, 2, 3]}
+
+def change_pos(data):
+    num = 0
+    cursor = db.history.find()
+    print(cursor)
+    for result in cursor:
+        result.pop("_id")
+    # print(result)
+    # print(result['historytrack'][1]['pos'])
+    # print(len(result['historytrack']))
+    while num < len(result['historytrack']):
+        list = result['historytrack'][num]['pos']
+        poslist.append(list)
+        num = num + 1
+    poslist[data["tag"]] = data["location"]
+    # print(poslist)
+
+        
+def save_history(poslist):
     num = 0
     import time
     time1 = time.time()
     list1 = []
     dic1 = {}
     dic2 = {}
-    while num < len(test):
+    while num < len(poslist):
         dic1["name"] = "Tag" + str(num)
-        dic1["pos"] = test[num]
+        dic1["pos"] = poslist[num]
         dic1["time"] = time1
         print(num)
         print(dic1)
