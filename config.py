990102d99f7c7206d566c8930a4c8414f3c7bf1d
gu <1964100035@qq.com>
2018-08-27 17:04:34 +0800

    test

--- config.py
+++ config.py
@@ -2,12 +2,111 @@ import json
 import pymongo
 import time
 
-
 # MongoDB client URL and port
 DEFAULT_URL = "mongodb://localhost:27017"
 
 client = pymongo.MongoClient(DEFAULT_URL)
 db = client['orion']
+
+
+history = {
+    "historytrack": [
+        {
+            "time": 1534142258.264,
+            "name": "Tag0",
+            "pos": [
+                10,
+                10,
+                10
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag1",
+            "pos": [
+                10,
+                10,
+                100
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag2",
+            "pos": [
+                20,
+                20,
+                2
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag3",
+            "pos": [
+                30,
+                30,
+                30
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag4",
+            "pos": [
+                40,
+                40,
+                40
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag5",
+            "pos": [
+                50,
+                5,
+                50
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag6",
+            "pos": [
+                60,
+                60,
+                60
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag7",
+            "pos": [
+                70,
+                70,
+                7
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag8",
+            "pos": [
+                8,
+                80,
+                80
+            ]
+        },
+        {
+            "time": 1534142258.264,
+            "name": "Tag9",
+            "pos": [
+                9,
+                90,
+                90
+            ]
+        }
+    ]
+}
+
+
+def create_history():
+    db.history.insert(history)
 
 
 def save_history(totallist):
@@ -28,6 +127,7 @@ def save_history(totallist):
 
 
 def change_pos(data):
+    create_history()
     totallist = []
     num = 0
     cursor = db.history.find()
@@ -41,10 +141,10 @@ def change_pos(data):
     save_history(totallist)
 
 
-
-
 def load_config():
     cursor = db.config.find()
     for result in cursor:
         result.pop("_id")
     return result
+
+
