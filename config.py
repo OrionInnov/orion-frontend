469a6a60d1e21a07299802b3e2a25c4e7675d3c2
jkddw <1561068317@qq.com>
2018-09-26 15:12:41 +0800

    Modified.

--- config.py
+++ config.py
@@ -63,11 +63,14 @@ def change_pos(data):
     save_history(totallist)
 
 
-def load_config():
-    cursor = db.config.find()
-    for result in cursor:
-        result.pop("_id")
+def load_config(DEBUG_CONFIG=True):
+    if DEBUG_CONFIG:
+        path = os.path.join(app.root_path, "config_test.json")
+        with open(path, "r") as f:
+            result = json.load(f)[0]
+    else:
+        cursor = db.config.find()
+        for result in cursor:
+            result.pop("_id")
+
     return result
-
-
-
