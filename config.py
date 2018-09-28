b920a9e95e6bff1f7f65858ab95e7fae8c08fa3c
jkddw <1561068317@qq.com>
2018-09-28 15:57:26 +0800

    Modified.

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import time
 import os
 
 from flask import request
+from . import app
 
 # MongoDB client URL and port
 DEFAULT_URL = "mongodb://localhost:27017"
@@ -15,19 +16,17 @@ db = client['orion']
 
 def create_history():
     create_time = time.time()
-    num = 0
     list_historytrack = []
     dic_part = {}
     dic_all = {}
     cursor = db.config.find()
     for result in cursor:
-        while num < len(result['tags']):
+        for num in range(len(result["tags"])):
             dic_part["name"] = "Tag" + str(num)
             dic_part["pos"] = []
             dic_part["time"] = create_time
             list_historytrack.append(dic_part)
             dic_part = {}
-            num = num + 1
         dic_all["historytrack"] = list_historytrack
     db.history.insert(dic_all, check_keys=False)
 
@@ -35,31 +34,27 @@ def create_history():
 
 def save_history(totallist):
     change_time = time.time()
-    num = 0
     list_historytrack = []
     dic_part = {}
     dic_all = {}
-    while num < len(totallist):
+    for num in range(len(totallist)):
         dic_part["name"] = "Tag" + str(num)
         dic_part["pos"] = totallist[num]
         dic_part["time"] = change_time
         list_historytrack.append(dic_part)
         dic_part = {}
-        num = num + 1
     dic_all["historytrack"] = list_historytrack
     db.history.insert(dic_all, check_keys=False)
 
 
 def change_pos(data):
     totallist = []
-    num = 0
     cursor = db.history.find()
     for result in cursor:
         result.pop("_id")
-    while num < len(result['historytrack']):
+    for num < range(len(result['historytrack'])):
         poslist = result['historytrack'][num]['pos']
         totallist.append(poslist)
-        num = num + 1
     totallist[data["tag"]] = data["location"]
     save_history(totallist)
 
